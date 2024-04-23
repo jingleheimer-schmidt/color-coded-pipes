933c0a88cf646c9ffad820c1b6248d53422203e5
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-04-22 22:27:45 -0700

    change neon to colorized to match filenames

--- settings.lua
+++ settings.lua
@@ -4,7 +4,7 @@ data:extend{
         type = "string-setting",
         name = "color-coded-pipes-color-mode",
         setting_type = "startup",
-        default_value = "neon",
-        allowed_values = { "weathered", "neon" },
+        default_value = "colorized",
+        allowed_values = { "weathered", "colorized" },
     }
 }
