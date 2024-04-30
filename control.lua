29c58c42c431fa3fab7c11689594e172c8f029bb
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-04-29 23:27:24 -0700

    copy/paste command for testing

--- control.lua
+++ control.lua
@@ -224,6 +224,7 @@ end)
 -- -- --         fast_replace = true
 -- -- --     }
 -- -- -- end
+-- chat command to "unpaint" all the pipes
 
 -- -- -- for _, surface in pairs(game.surfaces) do
 -- -- --     local original_pipes = surface.find_entities_filtered{type="pipe"}
@@ -241,4 +242,27 @@ end)
 -- -- --             }
 -- -- --         end
 -- -- --     end
--- -- -- end
+-- -- -- end-- for _, pipe in pairs(game.player.surface.find_entities_filtered{type = "pipe"}) do
+--     pipe.order_upgrade{
+--         force = game.player.force,
+--         target = "pipe",
+--         player = game.player,
+--         direction = pipe.direction
+--     }
+-- end
+-- for _, pipe in pairs(game.player.surface.find_entities_filtered{type = "pipe-to-ground"}) do
+--     pipe.order_upgrade{
+--         force = game.player.force,
+--         target = "pipe-to-ground",
+--         player = game.player,
+--         direction = pipe.direction
+--     }
+-- end
+-- for _, storage_tank in pairs(game.player.surface.find_entities_filtered{type = "storage-tank"}) do
+--     storage_tank.order_upgrade{
+--         force = game.player.force,
+--         target = "storage-tank",
+--         player = game.player,
+--         direction = storage_tank.direction
+--     }
+-- end
