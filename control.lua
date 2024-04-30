8b2f760fe846142f12e841c3a498d33fe65924fd
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-04-29 23:25:53 -0700

    add fluid color map

--- control.lua
+++ control.lua
@@ -66,6 +66,16 @@
 --         end
 --     end
 -- end
+local fluid_to_color_map = {
+    ["water"] = "blue",
+    ["crude-oil"] = "black",
+    ["steam"] = "white",
+    ["heavy-oil"] = "red",
+    ["light-oil"] = "orange",
+    ["petroleum-gas"] = "purple",
+    ["sulfuric-acid"] = "yellow",
+    ["lubricant"] = "green",
+}
 
 
 -- -- local function get_closest_color(r, g, b)
