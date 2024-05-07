6fbb2a4ef30fb616124032300442ee1ac4d90abb
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-05-07 12:08:41 -0700

    fix storage tank

--- data-updates.lua
+++ data-updates.lua
@@ -375,111 +375,7 @@ local function create_fluid_color_pipe_entity(fluid_name, fluid_color)
     data:extend{ pipe }
 end
 
-local fluids = data.raw["fluid"]
-for _, fluid in pairs(fluids) do
-    local fluid_color = fluid.base_color
-    local fluid_name = fluid.name
-
-    if not (fluid_color.r and fluid_color.g and fluid_color.b) then
-        log("fluid " .. fluid_name .. " has no color")
-        goto next_fluid
-    end
-
-    -- local h, s, l = rgb_to_hsl(color.r, color.g, color.b)
-    -- -- -- l = 75
-    -- color.r, color.g, color.b = hsl_to_rgb(h, s, l)
-
-    fluid_color.a = 0.75
-
-    create_fluid_color_pipe_entity(fluid_name, fluid_color)
-
-    -- local pipe = table.deepcopy(data.raw["pipe"]["pipe"])
-    -- if not pipe then log("pipe entity not found") return end
-    -- local pipe_name = fluid_name .. "-pipe"
-    -- local pipe_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.pipe" } }
-    -- pipe.name = pipe_name
-    -- pipe.minable.result = pipe_name
-    -- for _, filename in pairs(pipe_filenames) do
-    --     local property_name = replace_dash_with_underscore(filename)
-    --     local original_layer = table.deepcopy(pipe.pictures[property_name]) ---@type data.Sprite
-    --     local overlay_layer = table.deepcopy(pipe.pictures[property_name]) ---@type data.Sprite
-    --     -- overlay_layer.filename = "__color-coded-pipes__/graphics/overlay-pipe-" .. filename .. "/overlay-hr-pipe-" .. filename .. "@0.5x.png"
-    --     -- overlay_layer.hr_version.filename = "__color-coded-pipes__/graphics/overlay-pipe-" .. filename .. "/overlay-hr-pipe-" .. filename .. ".png"
-    --     -- overlay_layer.tint = color
-    --     -- overlay_layer.hr_version.tint = color
-    --     -- -- pipe.pictures[property_name].layers = { overlay_layer, original_layer }
-    --     -- pipe.pictures[property_name].layers = { overlay_layer }
-
-    --     local layers = {
-    --         [2] = {
-    --             filename = "__color-coded-pipes__/graphics/overlay-pipe-" .. filename .. "/overlay-hr-pipe-" .. filename .. "@0.5x.png",
-    --             hr_version = {
-    --                 filename = "__color-coded-pipes__/graphics/overlay-pipe-" .. filename .. "/overlay-hr-pipe-" .. filename .. ".png",
-    --                 tint = fluid_color,
-    --                 size = original_layer.hr_version.size,
-    --                 shift = original_layer.hr_version.shift,
-    --                 scale = original_layer.hr_version.scale,
-    --                 x = original_layer.hr_version.x,
-    --                 y = original_layer.hr_version.y,
-    --                 position = original_layer.hr_version.position,
-    --                 width = original_layer.hr_version.width,
-    --                 height = original_layer.hr_version.height,
-    --                 blend_mode = "normal",
-    --                 -- draw_as_glow = true,
-    --             },
-    --             tint = fluid_color,
-    --             size = original_layer.size,
-    --             shift = original_layer.shift,
-    --             scale = original_layer.scale,
-    --             x = original_layer.x,
-    --             y = original_layer.y,
-    --             position = original_layer.position,
-    --             width = original_layer.width,
-    --             height = original_layer.height,
-    --             blend_mode = "normal",
-    --             -- draw_as_glow = true,
-    --         },
-    --         [1] = {
-    --             filename = original_layer.filename,
-    --             hr_version = {
-    --                 filename = original_layer.hr_version.filename,
-    --                 tint = original_layer.hr_version.tint,
-    --                 size = original_layer.hr_version.size,
-    --                 shift = original_layer.hr_version.shift,
-    --                 scale = original_layer.hr_version.scale,
-    --                 x = original_layer.hr_version.x,
-    --                 y = original_layer.hr_version.y,
-    --                 position = original_layer.hr_version.position,
-    --                 width = original_layer.hr_version.width,
-    --                 height = original_layer.hr_version.height,
-    --             },
-    --             tint = original_layer.tint,
-    --             size = original_layer.size,
-    --             shift = original_layer.shift,
-    --             scale = original_layer.scale,
-    --             x = original_layer.x,
-    --             y = original_layer.y,
-    --             position = original_layer.position,
-    --             width = original_layer.width,
-    --             height = original_layer.height,
-    --         }
-    --     }
-    --     pipe.pictures[property_name] = nil
-    --     pipe.pictures[property_name] = { layers = layers }
-    -- end
-    -- pipe.localised_name = pipe_localised_name
-    -- -- pipe.corpse = name .. "-pipe-remnants"
-    -- local icon_base = {
-    --     icon = pipe.icon,
-    --     icon_size = pipe.icon_size,
-    --     icon_mipmaps = pipe.icon_mipmaps
-    -- }
-    -- local icon_overlay = table.deepcopy(icon_base)
-    -- icon_overlay.tint = fluid_color
-    -- icon_overlay.icon = "__color-coded-pipes__/graphics/overlay-pipe-icon/overlay-pipe-icon.png"
-    -- pipe.icons = { icon_base, icon_overlay }
-
-    -- data:extend{ pipe }
+local function create_fluid_color_pipe_item(fluid_name, fluid_color)
 
     local pipe_item = table.deepcopy(data.raw["item"]["pipe"])
     if not pipe_item then log("pipe item not found") return end
@@ -497,9 +393,12 @@ for _, fluid in pairs(fluids) do
     item_icon_overlay.tint = fluid_color
     item_icon_overlay.icon = "__color-coded-pipes__/graphics/overlay-pipe-icon/overlay-pipe-icon.png"
     pipe_item.icons = { item_icon_base, item_icon_overlay }
-    -- pipe_item.order = pipe_item.order .. recipe_order[color]
+    pipe_item.order = pipe_item.order .. "-" .. (data.raw["fluid"][fluid_name].order or "")
     pipe_item.subgroup = "color-coded-pipe"
     data:extend{ pipe_item }
+end
+
+local function create_fluid_color_pipe_recipe(fluid_name, fluid_color)
 
     local pipe_recipe = table.deepcopy(data.raw["recipe"]["pipe"])
     if not pipe_recipe then log("pipe recipe not found") return end
@@ -518,6 +417,214 @@ for _, fluid in pairs(fluids) do
     end
     pipe_recipe.localised_name = pipe_recipe_localised_name
     data:extend{ pipe_recipe }
+end
+
+local function create_fluid_color_pipe_to_ground_entity(fluid_name, fluid_color)
+
+    local pipe_to_ground = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
+    if not pipe_to_ground then log("pipe-to-ground entity not found") return end
+    local pipe_to_ground_name = fluid_name .. "-pipe-to-ground"
+    local pipe_to_ground_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.pipe-to-ground" } }
+    pipe_to_ground.name = pipe_to_ground_name
+    pipe_to_ground.minable.result = pipe_to_ground_name
+    for _, filename in pairs(pipe_to_ground_filenames) do
+        local property_name = replace_dash_with_underscore(filename)
+        local original_layer = table.deepcopy(pipe_to_ground.pictures[property_name]) ---@type data.Sprite
+        local overlay_layer = table.deepcopy(pipe_to_ground.pictures[property_name]) ---@type data.Sprite
+        overlay_layer.filename = "__color-coded-pipes__/graphics/overlay-pipe-to-ground-" .. filename .. "/overlay-hr-pipe-to-ground-" .. filename .. "@0.5x.png"
+        overlay_layer.hr_version.filename = "__color-coded-pipes__/graphics/overlay-pipe-to-ground-" .. filename .. "/overlay-hr-pipe-to-ground-" .. filename .. ".png"
+        overlay_layer.tint = fluid_color
+        overlay_layer.hr_version.tint = fluid_color
+        pipe_to_ground.pictures[property_name] = {}
+        pipe_to_ground.pictures[property_name].layers = { original_layer, overlay_layer }
+    end
+    pipe_to_ground.localised_name = pipe_to_ground_localised_name
+    -- pipe_to_ground.corpse = name .. "-pipe-to-ground-remnants"
+    local icon_base = {
+        icon = pipe_to_ground.icon,
+        icon_size = pipe_to_ground.icon_size,
+        icon_mipmaps = pipe_to_ground.icon_mipmaps
+    }
+    local icon_overlay = table.deepcopy(icon_base)
+    icon_overlay.tint = fluid_color
+    icon_overlay.icon = "__color-coded-pipes__/graphics/overlay-pipe-to-ground-icon/overlay-pipe-to-ground-icon.png"
+    pipe_to_ground.icons = { icon_base, icon_overlay }
+    data:extend{ pipe_to_ground }
+end
+
+local function create_fluid_color_pipe_to_ground_item(fluid_name, fluid_color)
+
+    local pipe_to_ground_item = table.deepcopy(data.raw["item"]["pipe-to-ground"])
+    if not pipe_to_ground_item then log("pipe-to-ground item not found") return end
+    local pipe_to_ground_item_name = fluid_name .. "-pipe-to-ground"
+    local pipe_to_ground_item_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.pipe-to-ground" } }
+    pipe_to_ground_item.name = pipe_to_ground_item_name
+    pipe_to_ground_item.place_result = pipe_to_ground_item_name
+    pipe_to_ground_item.localised_name = pipe_to_ground_item_localised_name
+    local item_icon_base = {
+        icon = pipe_to_ground_item.icon,
+        icon_size = pipe_to_ground_item.icon_size,
+        icon_mipmaps = pipe_to_ground_item.icon_mipmaps
+    }
+    local item_icon_overlay = table.deepcopy(item_icon_base)
+    item_icon_overlay.tint = fluid_color
+    item_icon_overlay.icon = "__color-coded-pipes__/graphics/overlay-pipe-to-ground-icon/overlay-pipe-to-ground-icon.png"
+    pipe_to_ground_item.icons = { item_icon_base, item_icon_overlay }
+    -- pipe_to_ground_item.order = pipe_to_ground_item.order .. recipe_order[color]
+    pipe_to_ground_item.subgroup = "color-coded-pipe-to-ground"
+    data:extend{ pipe_to_ground_item }
+end
+
+local function create_fluid_color_pipe_to_ground_recipe(fluid_name, fluid_color)
+
+    local pipe_to_ground_recipe = table.deepcopy(data.raw["recipe"]["pipe-to-ground"])
+    if not pipe_to_ground_recipe then log("pipe-to-ground recipe not found") return end
+    local pipe_to_ground_recipe_name = fluid_name .. "-pipe-to-ground"
+    local pipe_to_ground_recipe_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.pipe-to-ground" } }
+    pipe_to_ground_recipe.name = pipe_to_ground_recipe_name
+    pipe_to_ground_recipe.result = pipe_to_ground_recipe.result and pipe_to_ground_recipe_name or nil
+    pipe_to_ground_recipe.results = pipe_to_ground_recipe.results and { { type = "item", name = pipe_to_ground_recipe_name, amount = 1 } } or nil
+    if pipe_to_ground_recipe.normal then
+        pipe_to_ground_recipe.normal.result = pipe_to_ground_recipe.normal.result and pipe_to_ground_recipe_name or nil
+        pipe_to_ground_recipe.normal.results = pipe_to_ground_recipe.normal.results and { { type = "item", name = pipe_to_ground_recipe_name, amount = 1 } } or nil
+    end
+    if pipe_to_ground_recipe.expensive then
+        pipe_to_ground_recipe.expensive.result = pipe_to_ground_recipe.expensive.result and pipe_to_ground_recipe_name or nil
+        pipe_to_ground_recipe.expensive.results = pipe_to_ground_recipe.expensive.results and { { type = "item", name = pipe_to_ground_recipe_name, amount = 1 } } or nil
+    end
+    pipe_to_ground_recipe.localised_name = pipe_to_ground_recipe_localised_name
+    data:extend{ pipe_to_ground_recipe }
+end
+
+local function create_fluid_color_storage_tank_entity(fluid_name, fluid_color)
+
+    local storage_tank = table.deepcopy(data.raw["storage-tank"]["storage-tank"])
+    if not storage_tank then log("storage-tank entity not found") return end
+    local storage_tank_name = fluid_name .. "-storage-tank"
+    local storage_tank_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.storage-tank" } }
+    storage_tank.name = storage_tank_name
+    storage_tank.minable.result = storage_tank_name
+    local base_sheet = table.deepcopy(storage_tank.pictures.picture.sheets[1])
+    local shadow_sheet = table.deepcopy(storage_tank.pictures.picture.sheets[2])
+    local overlay_sheet = table.deepcopy(base_sheet)
+    overlay_sheet.filename = "__color-coded-pipes__/graphics/overlay-storage-tank/overlay-storage-tank.png"
+    overlay_sheet.hr_version.filename = "__color-coded-pipes__/graphics/overlay-storage-tank/overlay-hr-storage-tank.png"
+    overlay_sheet.tint = fluid_color
+    overlay_sheet.hr_version.tint = fluid_color
+    storage_tank.pictures.picture.sheets = {
+        [2] = overlay_sheet,
+        [1] = base_sheet,
+        [3] = shadow_sheet
+    }
+    storage_tank.localised_name = storage_tank_localised_name
+    -- storage_tank.corpse = color .. "-storage-tank-remnants"
+    local base_icon = {
+        icon = storage_tank.icon,
+        icon_size = storage_tank.icon_size,
+        icon_mipmaps = storage_tank.icon_mipmaps
+    }
+    local overlay_icon = table.deepcopy(base_icon)
+    overlay_icon.tint = fluid_color
+    overlay_icon.icon = "__color-coded-pipes__/graphics/overlay-storage-tank-icon/overlay-storage-tank-icon.png"
+    storage_tank.icons = { base_icon, overlay_icon }
+    data:extend{ storage_tank }
+end
+
+local function create_fluid_color_storage_tank_item(fluid_name, fluid_color)
+
+    local storage_tank = table.deepcopy(data.raw["item"]["storage-tank"])
+    if not storage_tank then log("storage-tank item not found") return end
+    local storage_tank_name = fluid_name .. "-storage-tank"
+    local storage_tank_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.storage-tank" } }
+    storage_tank.name = storage_tank_name
+    storage_tank.place_result = storage_tank_name
+    storage_tank.localised_name = storage_tank_localised_name
+    local item_icon_base = {
+        icon = storage_tank.icon,
+        icon_size = storage_tank.icon_size,
+        icon_mipmaps = storage_tank.icon_mipmaps
+    }
+    local item_icon_overlay = table.deepcopy(item_icon_base)
+    item_icon_overlay.tint = fluid_color
+    item_icon_overlay.icon = "__color-coded-pipes__/graphics/overlay-storage-tank-icon/overlay-storage-tank-icon.png"
+    storage_tank.icons = { item_icon_base, item_icon_overlay }
+    -- storage_tank.order = storage_tank.order .. recipe_order[color]
+    storage_tank.subgroup = "color-coded-storage-tank"
+    data:extend{ storage_tank }
+end
+
+local function create_fluid_color_storage_tank_recipe(fluid_name, fluid_color)
+
+    local storage_tank = table.deepcopy(data.raw["recipe"]["storage-tank"])
+    if not storage_tank then log("storage-tank recipe not found") return end
+    local storage_tank_name = fluid_name .. "-storage-tank"
+    local storage_tank_localised_name = { "", { "fluid-name." .. fluid_name }, " ", { "entity-name.storage-tank" } }
+    storage_tank.name = storage_tank_name
+    storage_tank.result = storage_tank.result and storage_tank_name or nil
+    storage_tank.results = storage_tank.results and { { type = "item", name = storage_tank_name, amount = 1 } } or nil
+    if storage_tank.normal then
+        storage_tank.normal.result = storage_tank.normal.result and storage_tank_name or nil
+        storage_tank.normal.results = storage_tank.normal.results and { { type = "item", name = storage_tank_name, amount = 1 } } or nil
+    end
+    if storage_tank.expensive then
+        storage_tank.expensive.result = storage_tank.expensive.result and storage_tank_name or nil
+        storage_tank.expensive.results = storage_tank.expensive.results and { { type = "item", name = storage_tank_name, amount = 1 } } or nil
+    end
+    storage_tank.localised_name = storage_tank_localised_name
+    for _, technology in pairs(data.raw["technology"]) do
+        if technology.effects then
+            for _, effect in pairs(technology.effects) do
+                if effect.type == "unlock-recipe" and effect.recipe == "storage-tank" then
+                    table.insert(technology.effects, { type = "unlock-recipe", recipe = storage_tank_name })
+                end
+            end
+        end
+        if technology.normal and technology.normal.effects then
+            for _, effect in pairs(technology.normal.effects) do
+                if effect.type == "unlock-recipe" and effect.recipe == "storage-tank" then
+                    table.insert(technology.normal.effects, { type = "unlock-recipe", recipe = storage_tank_name })
+                end
+            end
+        end
+        if technology.expensive and technology.expensive.effects then
+            for _, effect in pairs(technology.expensive.effects) do
+                if effect.type == "unlock-recipe" and effect.recipe == "storage-tank" then
+                    table.insert(technology.expensive.effects, { type = "unlock-recipe", recipe = storage_tank_name })
+                end
+            end
+        end
+    end
+    data:extend{ storage_tank }
+end
+
+local fluids = data.raw["fluid"]
+for _, fluid in pairs(fluids) do
+    local fluid_color = fluid.base_color
+    local fluid_name = fluid.name
+
+    if not (fluid_color.r and fluid_color.g and fluid_color.b) then
+        log("fluid " .. fluid_name .. " has no color")
+        goto next_fluid
+    end
+
+    -- local h, s, l = rgb_to_hsl(color.r, color.g, color.b)
+    -- -- -- l = 75
+    -- color.r, color.g, color.b = hsl_to_rgb(h, s, l)
+
+    fluid_color.a = 0.75
+
+    create_fluid_color_pipe_entity(fluid_name, fluid_color)
+    create_fluid_color_pipe_item(fluid_name, fluid_color)
+    create_fluid_color_pipe_recipe(fluid_name, fluid_color)
+
+    create_fluid_color_pipe_to_ground_entity(fluid_name, fluid_color)
+    create_fluid_color_pipe_to_ground_item(fluid_name, fluid_color)
+    create_fluid_color_pipe_to_ground_recipe(fluid_name, fluid_color)
+
+    create_fluid_color_storage_tank_entity(fluid_name, fluid_color)
+    create_fluid_color_storage_tank_item(fluid_name, fluid_color)
+    create_fluid_color_storage_tank_recipe(fluid_name, fluid_color)
+
 
     -- local pipe_to_ground = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
     -- if not pipe_to_ground then log("pipe-to-ground entity not found") return end
