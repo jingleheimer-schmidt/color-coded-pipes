7d24257c98041b7811c68c666ac01bc7905381bf
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-05-29 14:52:11 -0700

    modify default colors

--- settings.lua
+++ settings.lua
@@ -16,24 +16,18 @@ data:extend{
     }
 }
 
-local a = 0.6
+local a = 0.55
 local rgb_colors = {
     red =    { r = 0.9, g = 0.2, b = 0.2, a = a },
     orange = { r = 0.9, g = 0.5, b = 0.2, a = a },
     yellow = { r = 0.9, g = 0.9, b = 0.2, a = a },
-    green =  { r = 0.2, g = 0.9, b = 0.2, a = a },
-    blue =   { r = 0.2, g = 0.2, b = 0.9, a = a },
-    purple = { r = 0.5, g = 0.2, b = 0.9, a = a },
-    pink =   { r = 0.9, g = 0.2, b = 0.9, a = a },
+    green =  { r = 0.2, g = 0.8, b = 0.2, a = a },
+    blue =   { r = 0.1, g = 0.3, b = 0.7, a = a },
+    purple = { r = 0.6, g = 0.2, b = 0.9, a = a },
+    pink =   { r = 0.8, g = 0.2, b = 0.6, a = a },
     black =  { r = 0.2, g = 0.2, b = 0.2, a = a },
     white =  { r = 0.9, g = 0.9, b = 0.9, a = a },
 }
-
-for _, color in pairs(rgb_colors) do
-    color.r = color.r * 0.75
-    color.g = color.g * 0.75
-    color.b = color.b * 0.75
-end
 
 local recipe_order = {
     ["red"] = "a",
