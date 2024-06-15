8fae99a22688b784d4e6aa079aa654a897039baa
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-06-14 22:36:34 -0700

    fork planner tool to it's own mod

--- settings.lua
+++ settings.lua
@@ -55,27 +55,6 @@ for name, color in pairs(rgb_colors) do
     data:extend{color_setting}
 end
 
--- data:extend{
---     {
---         type = "bool-setting",
---         name = "color-coded-pipes-planner-tooltip",
---         setting_type = "runtime-per-user",
---         default_value = false,
---         order = "a",
---         hidden = true,
---     }
--- }
-
-data:extend{
-    {
-        type = "bool-setting",
-        name = "color-coded-pipes-bots-required",
-        setting_type = "runtime-per-user",
-        default_value = true,
-        order = "a",
-    }
-}
-
 data:extend{
     {
         type = "bool-setting",
