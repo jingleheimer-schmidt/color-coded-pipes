42ef150f409ccad3fd63ea871b0e56352b621f32
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-06-30 15:51:42 -0700

    prettify

--- settings.lua
+++ settings.lua
@@ -37,15 +37,15 @@ data:extend { color_mode, menu_simulations, rainbow_recipes, fluid_recipes }
 
 local a = 0.6
 local rgb_colors = {
-    red =    { r = 0.9, g = 0.2, b = 0.2, a = a },
+    red = { r = 0.9, g = 0.2, b = 0.2, a = a },
     orange = { r = 0.9, g = 0.5, b = 0.2, a = a },
     yellow = { r = 0.9, g = 0.9, b = 0.2, a = a },
-    green =  { r = 0.2, g = 0.8, b = 0.2, a = a },
-    blue =   { r = 0.1, g = 0.3, b = 0.7, a = a },
+    green = { r = 0.2, g = 0.8, b = 0.2, a = a },
+    blue = { r = 0.1, g = 0.3, b = 0.7, a = a },
     purple = { r = 0.6, g = 0.2, b = 0.9, a = a },
-    pink =   { r = 0.8, g = 0.2, b = 0.6, a = a },
-    black =  { r = 0.2, g = 0.2, b = 0.2, a = a },
-    white =  { r = 0.9, g = 0.9, b = 0.9, a = a },
+    pink = { r = 0.8, g = 0.2, b = 0.6, a = a },
+    black = { r = 0.2, g = 0.2, b = 0.2, a = a },
+    white = { r = 0.9, g = 0.9, b = 0.9, a = a },
 }
 
 local order = {
