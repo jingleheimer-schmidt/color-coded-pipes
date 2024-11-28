4fd11b84732a22b4c08004d81cc127c10ae4c6c0
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-11-27 22:48:40 -0800

    Create ps-export-1.0.4.js

---
+++ graphics/ps-export-1.0.4.js
@@ -0,0 +1,48 @@
+// Get all open documents
+var docs = app.documents;
+
+for (var d = 0; d < docs.length; d++) {
+    var doc = docs[d];
+    app.activeDocument = doc; // Activate the document
+    var layers = doc.layers;
+    var layers_length = layers.length;
+
+    // hide all layers
+    for (var i = 0; i < layers_length; i++) {
+        var layer = layers[i];
+        layer.visible = false;
+    }
+
+    for (var i = 0; i < layers_length; i++) {
+        var layer = layers[i];
+
+        // show the layer if it's names "overlay"
+        if (layer.name == "overlay") {
+            layer.visible = true;
+        } else {
+            continue;
+        }
+
+        // Export the .png file
+        var layerName = layer.name + "-";
+        var fileName = layerName + doc.name + ".png"; // overlay-<document_name>.png
+        fileName = fileName.replace(".psd", ""); // Remove .psd extension (if it exists)
+
+        // Export full resolution PNG
+        var file = new File(doc.path + "/" + fileName);
+        var exportOptions = new ExportOptionsSaveForWeb();
+        exportOptions.format = SaveDocumentType.PNG;
+        exportOptions.PNG8 = false;
+        exportOptions.transparency = true;
+        doc.exportDocument(file, ExportType.SAVEFORWEB, exportOptions);
+    };
+
+    // show all the layers again
+    for (var i = 0; i < layers_length; i++) {
+        var layer = layers[i];
+        layer.visible = true;
+    }
+
+    // save the document
+    doc.save();
+}
