1e2915bd7ab237ca7bb8f61b12d72dc66f9933b4
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2025-10-22 23:53:08 -0700

    Move mix_color function to functions.lua
    The mix_color function was relocated from constants.lua to functions.lua for better organization and modularity. All references to mix_color are now updated to use the new location.

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -3,6 +3,7 @@ local util = require("util")
 local functions = require("__color-coded-pipes__.scripts.functions")
 local append = functions.append
 local get_color = functions.get_color
+local mix_color = functions.mix_color
 
 -------------------------------------
 --- list of pipe sprite filenames ---
@@ -300,29 +301,6 @@ local function get_closest_named_color(color)
     end
 
     return closest_name
-end
-
--- Mixes two colors by a given percentage (0 - 1)
----@param c1 Color
----@param c2 Color
----@param percent number
----@return Color
-local function mix_color(c1, c2, percent)
-    percent = percent or 0.5
-    local r1 = c1.r or c1[1] or 0
-    local g1 = c1.g or c1[2] or 0
-    local b1 = c1.b or c1[3] or 0
-    local a1 = c1.a or c1[4] or 1
-    local r2 = c2.r or c2[1] or 0
-    local g2 = c2.g or c2[2] or 0
-    local b2 = c2.b or c2[3] or 0
-    local a2 = c2.a or c2[4] or 1
-    return {
-        r = (r1 * (1 - percent) + r2 * percent),
-        g = (g1 * (1 - percent) + g2 * percent),
-        b = (b1 * (1 - percent) + b2 * percent),
-        a = (a1 * (1 - percent) + a2 * percent),
-    }
 end
 
 ----------------------------------------------
--- scripts/functions.lua
+++ scripts/functions.lua
@@ -23,8 +23,32 @@ local function get_color(setting)
     return value
 end
 
+-- Mixes two colors by a given percentage (0 - 1)
+---@param c1 Color
+---@param c2 Color
+---@param percent number
+---@return Color
+local function mix_color(c1, c2, percent)
+    percent = percent or 0.5
+    local r1 = c1.r or c1[1] or 0
+    local g1 = c1.g or c1[2] or 0
+    local b1 = c1.b or c1[3] or 0
+    local a1 = c1.a or c1[4] or 1
+    local r2 = c2.r or c2[1] or 0
+    local g2 = c2.g or c2[2] or 0
+    local b2 = c2.b or c2[3] or 0
+    local a2 = c2.a or c2[4] or 1
+    return {
+        r = (r1 * (1 - percent) + r2 * percent),
+        g = (g1 * (1 - percent) + g2 * percent),
+        b = (b1 * (1 - percent) + b2 * percent),
+        a = (a1 * (1 - percent) + a2 * percent),
+    }
+end
+
 return {
     replace_dash_with_underscore = replace_dash_with_underscore,
     append = append,
     get_color = get_color,
+    mix_color = mix_color,
 }
