b8230218785e4995e94b8d828f124e67b2609822
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2026-02-10 00:47:50 -0800

    Move k2-fluid-storage hr graphics modification to compatibility file
    https://mods.factorio.com/mod/color-coded-pipes/discussion/6989f5735fbc5be8122731ba

--- data-updates.lua
+++ data-updates.lua
@@ -79,28 +79,6 @@ for _, base in pairs(base_entities) do
     end
 end
 
-------------------------------------------------------------
--- force the game to use the k2-fluid-storage hr graphics --
-------------------------------------------------------------
-
-if mods["k2-fluid-storage"] then
-    local tank_1 = data.raw["storage-tank"]["kr-fluid-storage-1"]
-    if tank_1 and tank_1.pictures and tank_1.pictures.picture and tank_1.pictures.picture.sheets then
-        for id, sheet in pairs(tank_1.pictures.picture.sheets) do
-            if sheet.hr_version then
-                data.raw["storage-tank"]["kr-fluid-storage-1"].pictures.picture.sheets[id] = sheet.hr_version
-            end
-        end
-    end
-    local tank_2 = data.raw["storage-tank"]["kr-fluid-storage-2"]
-    if tank_2 and tank_2.pictures and tank_2.pictures.picture and tank_2.pictures.picture.sheets then
-        for id, sheet in pairs(tank_2.pictures.picture.sheets) do
-            if sheet.hr_version then
-                data.raw["storage-tank"]["kr-fluid-storage-2"].pictures.picture.sheets[id] = sheet.hr_version
-            end
-        end
-    end
-end
 
 ------------------------------------
 -- just a couple helper functions --
--- scripts/compatibility.lua
+++ scripts/compatibility.lua
@@ -21,3 +21,26 @@ if mods["nulls-k2so-tweaks"] then
     replace_k2_fluid("kr-nitric-acid", "nitric-acid")
 end
 
+
+----------------------------------------------------
+-- force k2-fluid-storage to use it's hr graphics --
+----------------------------------------------------
+
+if mods["k2-fluid-storage"] then
+    local tank_1 = data.raw["storage-tank"]["kr-fluid-storage-1"]
+    if tank_1 and tank_1.pictures and tank_1.pictures.picture and tank_1.pictures.picture.sheets then
+        for id, sheet in pairs(tank_1.pictures.picture.sheets) do
+            if sheet.hr_version then
+                data.raw["storage-tank"]["kr-fluid-storage-1"].pictures.picture.sheets[id] = sheet.hr_version
+            end
+        end
+    end
+    local tank_2 = data.raw["storage-tank"]["kr-fluid-storage-2"]
+    if tank_2 and tank_2.pictures and tank_2.pictures.picture and tank_2.pictures.picture.sheets then
+        for id, sheet in pairs(tank_2.pictures.picture.sheets) do
+            if sheet.hr_version then
+                data.raw["storage-tank"]["kr-fluid-storage-2"].pictures.picture.sheets[id] = sheet.hr_version
+            end
+        end
+    end
+end
